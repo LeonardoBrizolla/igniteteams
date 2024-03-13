5f4600274947a273f3063bd82d4b87aaf99e6d4d
Leonardo Brizolla <leonardobrizollars@gmail.com>
2024-03-13 19:20:33 -0300

    Componente List Empty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -6,9 +6,10 @@ import { Highlight } from "@components/Highlight";
 import { GroupCards } from "@components/GroupCards";
 
 import { Container } from "./styles";
+import { ListEmpty } from "@components/ListEmpty";
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(["Galera da Rocket"]);
+  const [groups, setGroups] = useState<string[]>([]);
 
   return (
     <Container>
@@ -16,10 +17,14 @@ export function Groups() {
 
       <Highlight title="Turmas" subtitle="Jogue com sua turma" />
 
-      <FlatList 
-        data={groups} 
+      <FlatList
+        data={groups}
         keyExtractor={(item) => item}
         renderItem={({ item }) => <GroupCards title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
       />
     </Container>
   );
