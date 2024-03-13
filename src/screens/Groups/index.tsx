47ed367c0477bdd3d5bbbfc2b8d3ed4647c496a1
Leonardo Brizolla <leonardobrizollars@gmail.com>
2024-03-13 17:31:14 -0300

    criando componente highlight

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,14 @@
-import { Header } from '@components/Header';
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
 
-import { Container } from './styles'
+import { Container } from "./styles";
 
 export function Groups() {
-
   return (
     <Container>
       <Header />
+
+      <Highlight title="Turmas" subtitle="Jogue com sua turma" />
     </Container>
   );
 }
