ac44e425770461fe75cb85803c23bc8a364fd1c5
Leonardo Brizolla <leonardobrizollars@gmail.com>
2024-03-13 19:06:03 -0300

    Listando grupos

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
+
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { GroupCards } from "@components/GroupCards";
@@ -5,13 +8,19 @@ import { GroupCards } from "@components/GroupCards";
 import { Container } from "./styles";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(["Galera da Rocket"]);
+
   return (
     <Container>
       <Header />
 
       <Highlight title="Turmas" subtitle="Jogue com sua turma" />
 
-      <GroupCards title="Galera do Ignite"/>
+      <FlatList 
+        data={groups} 
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCards title={item} />}
+      />
     </Container>
   );
 }
