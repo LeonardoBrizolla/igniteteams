e7b08f8c2ad56f8be045e44815c5cf0bc29d9660
Leonardo Brizolla <leonardobrizollars@gmail.com>
2024-03-16 14:52:42 -0300

    Create ButtonIcon component

--- App.tsx
+++ App.tsx
@@ -2,14 +2,15 @@ import { StatusBar } from "react-native";
 import { ThemeProvider } from "styled-components";
 import { Groups } from "@screens/Groups";
 import { NewGroup } from "@screens/NewGroup";
-import theme from "./src/theme";
+import { Players } from "@screens/Players";
+import { Loading } from "@components/Loading";
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 
-import { Loading } from "@components/Loading";
+import theme from "./src/theme";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -21,7 +22,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <NewGroup /> : <Loading />}
+      {fontsLoaded ? <Players /> : <Loading />}
     </ThemeProvider>
   );
 }
